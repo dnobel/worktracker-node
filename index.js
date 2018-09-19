d527dea37a6824b2dadc6f0067aa8bf140589cc0
Dennis Nobel <dennis@dnobel.de>
2018-09-19 22:57:20 +0200

    Added parsing of dates.

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const express = require('express');
 const Datastore = require('@google-cloud/datastore');
 const datastore = Datastore();
 const app = express();
+const moment = require('moment');
 
 app.use(express.json());
-app.post('/', function(req, res) {
+app.post('/ifttt/area', function(req, res) {
+	
 	const entryKey = datastore.key(['Event']);
 	const payload = req.body;
-
+	const date = moment(payload.at, 'MMM DD, YYYY at hh:mm A');
+	
 	const entry = {
 		key: entryKey,
 		data: {
 			'location': payload.location,
 			'type': payload.type,
-			'date': new Date()
+			'date': date.toDate()
 		}
 	};
-
 
 	datastore
 		.save(entry)
 		.then(() => {
 			console.log(`Saved entity: ` + entry);
+			newEntryAdded(entry);
 			res.status(200).send(entry);
 		})
 		.catch(err => {
@@ -33,3 +36,7 @@ app.post('/', function(req, res) {
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
+
+function newEntryAdded(entry) {
+	console.log("New " + entry);
+}
